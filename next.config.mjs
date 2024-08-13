10d604e9e4ba66e6377bf237b1f8153a19cc9017
ebrivera <ebrivera@bu.edu>
2024-08-12 17:24:33 -0700

    removing the static from next config js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   distDir: ".next", // Ensures that the build output is placed in the .next directory
   reactStrictMode: true, // Enables React's Strict Mode to help identify potential issues
   trailingSlash: true, // Ensures that all paths have a trailing slash, which might be helpful for Firebase Hosting
