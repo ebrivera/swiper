6aa4763c3d54369f638b2d756fac4c8cd6b05b41
ebrivera <ebrivera@bu.edu>
2024-08-12 17:09:00 -0700

    trying to fix the deployment issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  distDir: ".next", // Ensures that the build output is placed in the .next directory
+  reactStrictMode: true, // Enables React's Strict Mode to help identify potential issues
+  trailingSlash: true, // Ensures that all paths have a trailing slash, which might be helpful for Firebase Hosting
+  // other configurations can be added here
+};
 
 export default nextConfig;
